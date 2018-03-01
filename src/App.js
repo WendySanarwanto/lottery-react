a8771397ad4d86b13bd6dc84530c7869268a3178
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-03-02 01:59:10 +0800

    Added logic & markups for allowing user to enter the game & updates displayed info after the user joined in successfully.

--- src/App.js
+++ src/App.js
@@ -9,17 +9,55 @@ class App extends Component {
     manager: '',
     balance: '',
     players: [],
-    value: ''
+    value: '',
+    enteringPlayerStatus: ''
   };
 
-  async componentDidMount(){
-    const manager = await lottery.methods.manager().call();
+  async pickGameInfo() {
     const players = await lottery.methods.getPlayers().call();
     const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({manager});
     this.setState({players});
-    this.setState({balance});
+    this.setState({balance});    
+  }
+
+  async componentDidMount(){
+    const manager = await lottery.methods.manager().call();
+    await this.pickGameInfo();
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();    
+    console.log(`[DEBUG] - <App.onSubmit> accounts: \n${JSON.stringify( accounts, " ", 2)}\nvalue: ${this.state.value}`);
+
+    const account = Array.isArray(accounts) && accounts.length > 0 ? accounts[0] : undefined;
+
+    if (!account) {
+      console.log(`[ERROR] - <App.onSubmit> cannot find available account.`);
+      return;
+    }
+
+    // TODO: Validate account's balance against entered value.
+
+    // TODO: Calling ethereum's method might take longer to complete. 
+    //       Therefore, we need to put some kind of processing feedback indicator to user.
+
+    try {
+      this.setState({enteringPlayerStatus: `Waiting for transaction success ... `});      
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, 'ether')
+      });
+
+      // TODO: When sending process is finished, close the processing feedback
+      //       And show some kind of visual feedback which tells the user that the process has been completed.
+      this.setState({enteringPlayerStatus: `You have entered the game.`});
+      await this.pickGameInfo();
+    } catch(err) {
+      this.setState({enteringPlayerStatus: `Entering the game is failing: ${err.message}`});
+    }
   }
 
   render() {
@@ -38,19 +76,28 @@ class App extends Component {
           </header>  
         </div>      
         <p>This contract is managed by {this.state.manager}</p>
-        <p>There are currently {this.state.players.length} people entered,</p>
-        <p>competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether.</p>
+        <p>There are currently <b>{this.state.players.length}</b> people entered,</p>
+        <p>competing to win <b>{web3.utils.fromWei(this.state.balance, 'ether')}</b> ether.</p>
         
         <hr/>
 
-        <form>
+        <form onSubmit={this.onSubmit}>
           <h4>Hello, do you want to play a game ?</h4>
+
           <div>
-            <label>Please enter amount of your ether, to enter the game</label>
+            <label>Please enter amount of your ether, to enter the game (>0.01 Ether)</label>
             &nbsp;&nbsp;<input value={this.state.value}
               onChange={event => this.setState({ value: event.target.value})} />
           </div>
+          <p>
+            <button>Enter</button>
+          </p>
+          
         </form>
+
+        <hr/>
+
+        <h1>{this.state.enteringPlayerStatus}</h1>
 
       </div>
     );
